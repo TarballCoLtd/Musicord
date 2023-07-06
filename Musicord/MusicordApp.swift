726dea487f37834cdfc5e970e344e46fe3778018
Tarball <alyx.ferrari@gmail.com>
2023-07-06 18:56:19 -0400

    Initial Commit

---
+++ Musicord/MusicordApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicordApp.swift
+//  Musicord
+//
+//  Created by tarball on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
